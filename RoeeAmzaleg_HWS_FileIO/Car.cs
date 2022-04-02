2a0597c822e48737dc5ccb28d4e20756103fda13
anak1 <anak1@HPSPECTRE>
2022-04-02 19:38:05 +0300

    Add project files.

---
+++ RoeeAmzaleg_HWS_FileIO/Car.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RoeeAmzaleg_HWS_FileIO
+{
+    public class Car
+    {
+        private int Codan;
+        protected int NumberOfSeats;
+        Car[] cars = new Car[0];    
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+
+        public Car()
+        {
+
+        }
+        public Car(string model, string brand, string color, int year, int numberOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Color = color;
+            Year = year;
+            NumberOfSeats = numberOfSeats;
+        }
+        public Car(string FileName)
+        {
+
+        }
+        public int GetCodan()
+        {
+            return Codan;
+        }
+        public int GetNumberOfSeats()
+        {
+            return NumberOfSeats;
+        }
+        protected void ChangeNumberOfSeats(int numOfSeats)
+        {
+            NumberOfSeats = numOfSeats;
+        }
+        public static void SerializeACar(string fileName, Car car)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Append))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Car)); 
+                xml.Serialize(stream, car);
+            }
+
+        }
+        public static void SerializeACarArray(string fileName, Car[] car)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Append))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Car[]));
+                xml.Serialize(stream, car);
+            }
+
+        }
+        public static Car DeSerializeCar(string fileName)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                XmlSerializer CarXml = new XmlSerializer(typeof(Car));
+                Car deSerializeCar = (Car)CarXml.Deserialize(stream);
+                return deSerializeCar;
+            }
+
+        }
+        public static Car[] DeSerializeCarArray(string fileName)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                XmlSerializer xmlArray = new XmlSerializer(typeof(Car[]));
+
+                Car[] deSerializeCar = (Car[])xmlArray.Deserialize(stream);
+                return deSerializeCar;
+            }
+
+        }
+        //public bool CarCompare(string filename)
+        //{
+
+
+        //}
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ RoeeAmzaleg_HWS_FileIO/Program.cs
@@ -0,0 +1,86 @@
+﻿using RoeeAmzaleg_HWS_FileIO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RoeeAmzaleg_HWS_FileIO
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXR 16 - Print 10 files from your computer
+            string[] allFiles = Directory.GetDirectories(@"C:\3uTools", "*.*", SearchOption.AllDirectories);
+            try
+            {
+                for (int i = 0; i < allFiles.Length; i++)
+                {
+                    Console.WriteLine(allFiles[i]);
+                }
+            }
+            catch (IndexOutOfRangeException) when (allFiles.Length < 10)
+            {
+                Console.WriteLine("there is no 10 files !");
+            }
+            //EXR 17  
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car));
+
+            //Serialize to computer file
+            using (Stream myFile = new FileStream(@"C:\MyFile\Car.txt", FileMode.Create))
+            {
+                Car c1 = new Car { Model = "Forester", Brand = "Subaru", Color = "Black", Year = 2022 };
+                xmlSerializer.Serialize(myFile, c1);
+            }
+            //Deserialize from computer file
+            using (Stream myFile = new FileStream(@"C:\MyFile\Car.txt", FileMode.Open))
+            {
+                Car ReadCarFromXml = (Car)xmlSerializer.Deserialize(myFile);
+            }
+            //Changing file with Xml
+            using (Stream myFile = new FileStream(@"C:\MyFile\Car.txt", FileMode.Append))
+            {
+                Car c2 = new Car { Model = "Monza SP2", Brand = "Ferrari", Color = "Red", Year = 2022 };
+                xmlSerializer.Serialize(myFile, c2);
+            }
+            //creating car array + Serialize to file
+            using (Stream arrayOfCar = new FileStream(@"C:\MyFile\Car.txt", FileMode.Open))
+            {
+                XmlSerializer arrayXmlSerializer = new XmlSerializer(typeof(Car[]));
+                Car[] carArray = new Car[3];
+                carArray[0] = new Car { Model = "F8 Spider", Brand = "Ferrri", Color = "Yellow", Year = 2021 };
+                carArray[1] = new Car { Model = "Virage", Brand = "Aston Martin", Color = "Black", Year = 2012 };
+                carArray[2] = new Car { Model = "Continental GT V8", Brand = "Bentley", Color = "Blue", Year = 2022 };
+
+                arrayXmlSerializer.Serialize(arrayOfCar, carArray);
+            }
+            //Deserialize from file
+            using (Stream arrayOfCar = new FileStream(@"C:\MyFile\Car.txt", FileMode.Open))
+            {
+                Car carsReadFromFile = (Car)xmlSerializer.Deserialize(arrayOfCar);
+            }
+
+            //EXR 17 - Print files methods
+        }
+        public static void PrintFilesMethods(string path)
+        {
+
+           
+            //FileInfo[] filesInfo = dir.GetFiles();,
+            DirectoryInfo dir = new DirectoryInfo(path);
+            List<FileInfo> files = dir.GetFiles().ToList();
+            foreach (var item in files)
+            {
+                var theBigestFile = dir.GetFiles().OrderByDescending(x => x.Length).Take(3).ToList();
+                var theLastUpdatedFiles = File.GetLastWriteTime(path);       
+            }
+        }
+    }
+}
+
